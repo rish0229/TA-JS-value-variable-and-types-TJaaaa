5cda09ab8031731e0ab1f0dd501d5d603240b884
rish0229 <sushantsareen456@gmail.com>
2020-08-17 07:39:13 +0530

    do as directed

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,33 +6,75 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+var user = prompt('name?');
+alert(user);
+var userName = user;
+console.log(user);
+console.log(userName);
+var age = 22,
+  isLearning = true;
+user = 'John';
+console.log(user, userName);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+var numA = Number(prompt('Enter first number?'));
+var numB = Number(prompt('Enter second number?'));
+var sum = numA + numB;
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j = j + 2) console.log(j);
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (var i = 10; i > 0; i--) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+
+let i = 5;
+let result = '';
+while (i >= 0) {
+  result = result + String(i);
+  i--;
+}
+console.log(result);
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
-// Comparisoin
+let i = 10;
+let result = '';
+while (i >= 0) {
+  if (i % 2 === 0) {
+    result = result + String(i);
+  }
+  i--;
+}
+console.log(result);
+
+// Comparison
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
-
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
-
-
 Example:
-
 21, 21; // true
 "hello", -21; // false
-
 */
+
+var num1 = prompt('Enter first value?');
+var num2 = prompt('Enter second value?');
+if (num1 === num2) {
+  alert('true');
+} else {
+  alert('false');
+}
